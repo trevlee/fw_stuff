027c3d4bb79d0e149d71bd0387fa826aa6cbf1a7
aleo <43550587+trevlee@users.noreply.github.com>
2022-03-24 21:36:03 -0400

    Update iptables-standard.sh

--- iptables-standard.sh
+++ iptables-standard.sh
@@ -4,8 +4,8 @@
 
 # set default policies to drop
 iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
 
 # flush old rules across the various tables
 iptables -F
@@ -31,28 +31,6 @@ iptables -A FORWARD -m state --state INVALID -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# input chain
-iptables -A INPUT -p tcp -m tcp --dport 22 -m comment --comment "SSH" -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 80 -m comment --comment "HTTP" -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -m comment --comment "HTTPS" -j ACCEPT
-
-# output chain
-iptables -A OUTPUT -p tcp -m tcp --dport 22 -m comment --comment "SSH" -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --dport 53 -m comment --comment "DNS-TCP" -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --dport 53 -m comment --comment "DNS-UDP" -j ACCEPT
-iptables -A OUTPUT -p udp -m udp --dport 67:68 -m comment --comment "DHCP" -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --dport 80 -m comment --comment "HTTP" -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --dport 443 -m comment --comment "HTTPS" -j ACCEPT
-
 # allow incoming icmp packets
 iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# allow outgoing icmp packets
-iptables -A OUTPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# explicit drop
-iptables -A INPUT -j DROP
-iptables -A OUTPUT -j DROP
-iptables -A FORWARD -j DROP
