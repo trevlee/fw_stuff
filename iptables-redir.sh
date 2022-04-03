7c579d91178e72ff5e80ada00276b70a328155a8
aleo <43550587+trevlee@users.noreply.github.com>
2022-04-03 15:47:11 -0400

    Update iptables-redir.sh

--- iptables-redir.sh
+++ iptables-redir.sh
@@ -37,7 +37,7 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 # output chain
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p udp ---dport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 67:68 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
