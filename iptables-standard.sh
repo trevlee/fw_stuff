89da3d5ba1a14c6486bcf136134e2578617febfc
aleo <43550587+trevlee@users.noreply.github.com>
2022-11-30 17:31:28 -0500

    Update iptables-standard.sh

--- iptables-standard.sh
+++ iptables-standard.sh
@@ -1,37 +1,45 @@
 #!/usr/bin/env bash
 
-# set default policies to drop
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+# iptables var
+ipt=$(which iptables)
+
+# set filter policies to drop
+$ipt -P INPUT DROP
+$ipt -P OUTPUT DROP
+$ipt -P FORWARD DROP
 
 # flush rules + chains across the various tables
 # flush packet counters across the various tables
-iptables -F
-iptables -X
-iptables -Z
-iptables -t nat -F
-iptables -t nat -X
-iptables -t nat -Z
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -t mangle -Z
+$ipt -t raw -F
+$ipt -t raw -X
+$ipt -t raw -Z
+$ipt -t mangle -F
+$ipt -t mangle -X
+$ipt -t mangle -Z
+$ipt -t nat -F
+$ipt -t nat -X
+$ipt -t nat -Z
+$ipt -t filter -F
+$ipt -t filter -X
+$ipt -t filter -Z
+$ipt -t security -F
+$ipt -t security -X
+$ipt -t security -Z
 
 # allow loopback traffic
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+$ipt -A INPUT -i lo -j ACCEPT
+$ipt -A OUTPUT -o lo -j ACCEPT
 
 # drop invalid packets
-iptables -A INPUT  -m state --state INVALID -j DROP
-iptables -A OUTPUT -m state --state INVALID -j DROP
-iptables -A FORWARD -m state --state INVALID -j DROP
+$ipt -A INPUT  -m state --state INVALID -j DROP
+$ipt -A OUTPUT -m state --state INVALID -j DROP
+$ipt -A FORWARD -m state --state INVALID -j DROP
 
 # input chain
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$ipt -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # output chain
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-iptables -A OUTPUT -p icmp -j ACCEPT
+$ipt -A OUTPUT -p tcp --dport 80 -j ACCEPT
+$ipt -A OUTPUT -p tcp --dport 443 -j ACCEPT
+$ipt -A OUTPUT -p udp --dport 53 -j ACCEPT
+$ipt -A OUTPUT -p icmp -j ACCEPT
